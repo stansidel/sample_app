2d8a7f0bf77808e23e0193acec238a1eede8ea6b
Sidelnikov Stanislav <sidelnikov.stanislav@gmail.com>
2013-02-07 21:05:15 +0600

    Till 7.1.4

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -10,4 +10,13 @@ describe "User pages" do
     it { should have_selector('h1',    text: 'Sign up') }
     it { should have_selector('title', text: full_title('Sign up')) }
   end
+
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+
+    before { visit user_path(user) }
+
+    it { should have_selector('h1', test: user.name) }
+    it { should have_selector('title', test: user.name) }
+  end
 end
