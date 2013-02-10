0a3af889fb67a46dc103ed9856425a9602170bd5
Sidelnikov Stanislav <sidelnikov.stanislav@gmail.com>
2013-02-10 23:45:27 +0600

    s

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -4,22 +4,23 @@ describe "Authentication" do
 
   subject { page }
 
-  describe "signin page" do
+  describe "signin" do
     before { visit signin_path }
 
-    describe "signin" do
-      before { visit signin_path }
+    describe "with invalid information" do
+      before do
+        fill_in "Email", with: "sss"
+        click_button "Sign in"
+      end
 
-      describe "with invalid information" do
-        before { click_button "Sign in" }
+      it { should have_selector('title', text: 'Sign in') }
+      it { should have_selector('div.alert.alert-error', text: 'Invalid') }
 
-        it { should have_selector('title', text: 'Sign in') }
-        it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+      #find_field('email').value == 'sss'
 
-        describe "after visiting another page" do
-          before { click_link "Home" }
-          it { should_not have_selector('div.alert.alert-error') }
-        end
+      describe "after visiting another page" do
+        before { click_link "Home" }
+        it { should_not have_selector('div.alert.alert-error') }
       end
     end
 
@@ -34,7 +35,7 @@ describe "Authentication" do
       it { should have_link('Profile', href: user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
-      
+
       describe "followed by signout" do
         before { click_link "Sign out" }
         it { should have_link('Sign in') }
